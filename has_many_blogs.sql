d618cfa57e5c28b3f8ac347979de2c4c7e9956b0
Owen <owenyangg@gmail.com>
2016-04-20 16:10:00 -1000

    adds psql folder

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -1,43 +1,43 @@
--- DROP USER IF EXISTS has_many_user;
+DROP USER IF EXISTS has_many_user;
 
--- CREATE USER has_many_user;
+CREATE USER has_many_user;
 
--- DROP DATABASE IF EXISTS has_many_blogs;
+DROP DATABASE IF EXISTS has_many_blogs;
 
--- CREATE DATABASE has_many_blogs OWNER has_many_user;
+CREATE DATABASE has_many_blogs;
 
--- \c has_many_blogs;
+\c has_many_blogs;
 
--- DROP TABLE IF EXISTS users CASCADE;
--- DROP TABLE IF EXISTS posts CASCADE;
--- DROP TABLE IF EXISTS comments CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS posts CASCADE;
+DROP TABLE IF EXISTS comments CASCADE;
 
--- CREATE TABLE users (
---   id SERIAL PRIMARY KEY,
---   username varchar(90) NOT NULL,
---   first_name varchar(90) default NULL,
---   last_name varchar(90) default NULL,
---   created_at timestamp with time zone default now() NOT NULL,
---   updated_at timestamp with time zone default now() NOT NULL
---   );
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username varchar(90) NOT NULL,
+  first_name varchar(90) default NULL,
+  last_name varchar(90) default NULL,
+  created_at timestamp with time zone default now() NOT NULL,
+  updated_at timestamp with time zone default now() NOT NULL
+  );
 
--- CREATE TABLE posts (
---   id SERIAL PRIMARY KEY,
---   title varchar(180) default NULL,
---   url varchar(510) default NULL,
---   content text default NULL,
---   created_at timestamp with time zone default now() NOT NULL,
---   updated_at timestamp with time zone default now() NOT NULL,
---   user_id integer REFERENCES users
---   );
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  title varchar(180) default NULL,
+  url varchar(510) default NULL,
+  content text default NULL,
+  created_at timestamp with time zone default now() NOT NULL,
+  updated_at timestamp with time zone default now() NOT NULL,
+  user_id integer REFERENCES users
+  );
 
--- CREATE TABLE comments (
---   id SERIAL PRIMARY KEY,
---   body varchar(510) default NULL,
---   created_at timestamp with time zone default now() NOT NULL,
---   updated_at timestamp with time zone default now() NOT NULL,
---   user_id integer REFERENCES users,
---   post_id integer REFERENCES posts
---   );
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY,
+  body varchar(510) default NULL,
+  created_at timestamp with time zone default now() NOT NULL,
+  updated_at timestamp with time zone default now() NOT NULL,
+  user_id integer REFERENCES users,
+  post_id integer REFERENCES posts
+  );
 
--- \i './scripts/blog_data.sql';
+\i './scripts/blog_data.sql';
